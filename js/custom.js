6440da524f7a5b538b7e5a464b7903d15a429f01
mistersakil <sakil.diu.cse@gmail.com>
2022-10-17 16:32:54 +0600

    added css global variables

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,25 @@
+/**
+ * Theme change
+ */
+const body = document.querySelector("body");
+const themeChangeIcon = document.querySelector("#themeChangeIcon");
+
+themeChangeIcon.onclick = function (event) {
+  let iconClassList = event.target.classList;
+  if (iconClassList.contains("fa-moon")) {
+    iconClassList.add("fa-sun");
+    iconClassList.remove("fa-moon");
+  } else {
+    iconClassList.add("fa-moon");
+    iconClassList.remove("fa-sun");
+  }
+  console.log(iconClassList);
+  body.classList.toggle("light");
+};
+
+/**
+ * Mobile menu toggle
+ */
 const menuBtn = document.querySelector("#menuBtn");
 const navbar = document.querySelector(".navbar");
 menuBtn.onclick = function () {
