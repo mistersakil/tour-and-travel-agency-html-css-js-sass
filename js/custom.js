43ab3d45aacee5e2bc54068ba6469c0ced1294ff
mistersakil <sakil.diu.cse@gmail.com>
2022-10-17 20:16:30 +0600

    theme switcher done

--- js/custom.js
+++ js/custom.js
@@ -4,9 +4,8 @@
 const body = document.querySelector("body");
 const themeChangeIcon = document.querySelector("#themeChangeIcon");
 
-themeChangeIcon.onclick = function (event) {
-  /* Change toggle icon */
-  let iconClassList = event.target.classList;
+const checkThemeIcon = function () {
+  let iconClassList = themeChangeIcon.classList;
   if (iconClassList.contains("fa-moon")) {
     iconClassList.add("fa-sun");
     iconClassList.remove("fa-moon");
@@ -14,12 +13,33 @@ themeChangeIcon.onclick = function (event) {
     iconClassList.add("fa-moon");
     iconClassList.remove("fa-sun");
   }
+};
+/** Check previous theme */
+let currentTheme = localStorage.getItem("theme")
+  ? localStorage.getItem("theme")
+  : 12;
+console.log(currentTheme);
 
+if (currentTheme == "light") {
+  body.classList.add("light");
+  checkThemeIcon();
+} else {
+  body.classList.remove("light");
+}
+
+themeChangeIcon.onclick = function (event) {
+  /* Check theme icon */
+  checkThemeIcon();
   /* Change theme */
-  if (body.classList.contains("light")) {
-    body.classList.remove("light");
+  let getTheme = localStorage.getItem("theme")
+    ? localStorage.getItem("theme")
+    : false;
+  if (getTheme == "light") {
+    body.classList.remove(getTheme);
+    localStorage.setItem("theme", false);
   } else {
     body.classList.add("light");
+    localStorage.setItem("theme", "light");
   }
 };
 
